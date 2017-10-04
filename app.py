57e9b03a27511370e49542fd9ef1dd3ba7baabc4
Alessandro Cartegni <acartegni@gmail.com>
2017-10-04 19:30:16 -0400

    comments and refinements

--- app.py
+++ app.py
@@ -9,26 +9,28 @@ app = Flask(__name__)
 
 app.secret_key = os.urandom(32)
 
+#root route pulls up login template
 @app.route("/")
 def hi():
     return render_template('login.html', message = "")
 
+#auth checks for the correct username and password
 @app.route("/auth", methods = ["POST"])
 def process():
-    person = request.form["User"]
-    pw = request.form["Pass"]
+    person = request.form["User"] #gets the entered username
+    pw = request.form["Pass"] #same for pw
     reqmeth = request.method
-    if (person == "KenM"):
+    if (person == "KenM"): #checks for correct info
         if (pw == "1234"):  
             session["username"] = person
-            return render_template('welcome.html', name = person, method = reqmeth)
-        else:
+            return render_template('welcome.html', name = person, method = reqmeth) #if theinfo is good, renders the welcome
+        else: #if user was correct but pw incorrect, back to login with message
             return render_template('login.html', message = "Invalid Password")
-    else:
+    else: #if ser incorrect, back to login with message
         return render_template('login.html', message = "Invalid Username")
 
 @app.route("/logout")
-def logout():
+def logout(): #ends session and brings back to login with message
     session.pop("username")
     return render_template('login.html', message = "Successfully logged out!")
 
